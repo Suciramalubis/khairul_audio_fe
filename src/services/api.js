1825ec9204363f1644cef407a592f46e531b18f8
Muhammad Attan <121651022+attmhd@users.noreply.github.com>
2026-06-17 15:51:58 +0700

    Change API base URL to relative path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.26.144.68/api", // sesuaikan backend kamu
+  baseURL: "/api", // sesuaikan backend kamu
 });
 
 export default api;
