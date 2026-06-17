a5f079311181e617975b8b5ac3e6eeb118ce0755
Muhammad Attan <121651022+attmhd@users.noreply.github.com>
2026-06-17 15:52:41 +0700

    Change API base URL to relative path

--- src/services/wishlistService.js
+++ src/services/wishlistService.js
@@ -1,7 +1,7 @@
 // src/services/wishlistService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = '/api';
 
 // Helper untuk mendapatkan token (sesuaikan dengan cara Anda menyimpan token)
 const getToken = () => localStorage.getItem('token');
